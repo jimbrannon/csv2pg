1179735f863b65b383383bad46f5e5c868bfc446
Jim Brannon <jim@brannondevelopments.com>
2015-08-20 12:16:34 -0600

    fixed some errors and it is working
    fixed some errors and now it is successfully parsing a file into an
    array of field arrays

--- csv2pg.php
+++ csv2pg.php
@@ -13,7 +13,7 @@ define("LOGGING","logging");
  * the difference is they all get stuffed into an options array, makes for much cleaner code
  */
 $options[FILENAME]="dirty_data_1.txt";
-$options[METHOD]=0;
+$options[METHOD]=1;
 $options[DEBUGGING]=true;
 $options[LOGGING]=true;
 /*
@@ -26,11 +26,11 @@ if (array_key_exists(LOGGING,$options)) {
 }
 if (csv2pg($options)) {
 	if ($logging) {
-		echo "It worked.\m";
+		echo "It worked.\n";
 	}
 } else {
 	if ($logging) {
-		echo "It failed.\m";
+		echo "It failed.\n";
 	}
 }
 
@@ -59,20 +59,20 @@ function csv2pg($options=array()) {
 	 * convert file into array of file records
 	 */
 	if (array_key_exists(FILENAME,$options)) {
-		$filename = $options[FILENAME];
+		$file_name = $options[FILENAME];
 	} else {
 		if ($logging) {
-			echo "Error: Missing file name.\n";
+			echo "Error: csv2pg: Missing file name.\n";
 		}
 		if ($debugging) {
-			echo "Options array:\n";
+			echo "Options array: csv2pg:\n";
 			print_r($options);
 		}
 		return false;
 	}
 	if ($file_records = file($file_name)) {
 		if ($debugging) {
-			echo "File record array:\n";
+			echo "File record array: csv2pg:\n";
 			print_r($file_records);
 		}
 		/*
@@ -85,29 +85,40 @@ function csv2pg($options=array()) {
 			/*
 			 * append field values to pg table
 			 */
+			return true;
 		} else {
 			if ($logging) {
-				echo "Error: could not convert file record array into file field arrays\n";
+				echo "Error: csv2pg: could not convert file record array into file field arrays\n";
 			}
 			return false;
 		}
 	} else {
 		if ($logging) {
-			echo "Error: could not read records from file: ".$file_name."\n";
+			echo "Error: csv2pg: could not read records from file: ".$file_name."\n";
 		}
 		return false;
 	}
 }
 
 function csv2array($file_records,$options=array()) {
+	if (array_key_exists(LOGGING,$options)) {
+		$logging = $options[LOGGING];
+	} else {
+		$logging = false;
+	}
+	if (array_key_exists(DEBUGGING,$options)) {
+		$debugging = $options[DEBUGGING];
+	} else {
+		$debugging = false;
+	}
 	if (array_key_exists(METHOD,$options)) {
 		$method = $options[METHOD];
 	} else {
 		if ($logging) {
-			echo "Error: missing file processing method\n";
+			echo "Error: csv2array: missing file processing method\n";
 		}
 		if ($debugging) {
-			echo "Options array:\n";
+			echo "Options array: csv2array:\n";
 			print_r($options);
 		}
 		return false;
@@ -118,7 +129,7 @@ function csv2array($file_records,$options=array()) {
 		case 0:
 		default:
 			if ($logging) {
-				echo "Error: invalid file processing method specified: ".$method."\n";
+				echo "Error: csv2array: invalid file processing method specified: ".$method."\n";
 			}
 			return false;
 	}
