72f6234ca6ad65bf507c1563e7d91f7f3aaa0fc3
Jim Brannon <jim@brannondevelopments.com>
2015-08-20 14:37:42 -0600

    added all args
    added all the args, but can;t test it with a php cli in windows 10,
    apparently.  will test it later in linux

--- csv2pg.php
+++ csv2pg.php
@@ -1,11 +1,19 @@
 <?php
 /*
+ * our awesome php utils
+ */
+include "misc_io.php";
+
+/*
  * constants
  */
 define("METHOD","method");
 define("FILENAME","filename");
 define("DEBUGGING","debugging");
 define("LOGGING","logging");
+define("SKIPLINES","skiplines");
+define("FIELDCOUNT","fieldcount");
+define("LINENUMBERS","linenumbers");
 /*
  * argument defaults
  * much like pg2gviz
@@ -16,6 +24,9 @@ $options[FILENAME]="dirty_data_1.txt";
 $options[METHOD]=1;
 $options[DEBUGGING]=true;
 $options[LOGGING]=true;
+$options[SKIPLINES]=0;
+$options[FIELDCOUNT]=0;
+$options[LINENUMBERS]=false;
 /*
  * the code 
  */
@@ -34,6 +45,9 @@ if (csv2pg($options)) {
 	}
 }
 
+/*
+ * the functions that are the working componenets of the engine for this simple DMI
+ */
 function csv2pg($options=array()) {
 	/*
 	 * get command line args here, replacing default values
@@ -41,38 +55,145 @@ function csv2pg($options=array()) {
 	 * in other words, defaults get used unless over-ridden by command line args
 	 * the difference is they all get stuffed into an options array, makes for much cleaner code
 	 */
-	
 	/*
-	 * now start the file processing
+	 * get the debugging arg
+	 */
+	if (array_key_exists(DEBUGGING,$options)) {
+		$debugging = $options[DEBUGGING];
+	} else {
+		$debugging = false;
+	}
+	if ($debugging) echo "debugging default: $debugging \n";
+	$debugging_arg = getargs ("debug",$debugging);
+	if ($debugging) echo "debugging_arg: $debugging_arg \n";
+	if (strlen(trim($debugging_arg))) {
+		$debugging = strtobool($debugging_arg);
+		$options[DEBUGGING] = $debugging;
+	}
+	if ($debugging) echo "debugging final: $debugging \n";
+	/*
+	 * get the logging arg
 	 */
 	if (array_key_exists(LOGGING,$options)) {
 		$logging = $options[LOGGING];
 	} else {
 		$logging = false;
 	}
-	if (array_key_exists(DEBUGGING,$options)) {
-		$debugging = $options[DEBUGGING];
-	} else {
-		$debugging = false;
+	if ($debugging) echo "logging default: $logging \n";
+	$logging_arg = getargs ("logging",$logging);
+	if ($debugging) echo "logging_arg: $logging_arg \n";
+	if (strlen(trim($logging_arg))) {
+		$logging = strtobool($logging_arg);
+		$options[LOGGING] = $logging;
 	}
+	if ($debugging) echo "logging final: $logging \n";
 	/*
-	 * convert file into array of file records
+	 * get the filename arg
+	 * this is required, so bail if it is not set from either the default above or the cli arg 
 	 */
 	if (array_key_exists(FILENAME,$options)) {
 		$file_name = $options[FILENAME];
 	} else {
-		if ($logging) {
-			echo "Error: csv2pg: Missing file name.\n";
-		}
-		if ($debugging) {
-			echo "Options array: csv2pg:\n";
-			print_r($options);
-		}
-		return false;
+		// we can NOT set a default for this so the arg better have something!
+		$file_name = "";
 	}
+	if ($debugging) echo "file_name default: $file_name \n";
+	$file_name_arg = getargs ("filename",$file_name);
+	if ($debugging) echo "file_name_arg: $file_name_arg \n";
+	if (strlen(trim($file_name_arg))) {
+		$file_name = trim($file_name_arg);
+		$options[FILENAME] = $file_name;
+		if ($debugging) echo "file_name final: $file_name \n";
+	} else {
+		if (strlen(trim($file_name))) {
+			if ($debugging) echo "file_name final: $file_name \n";
+		} else {
+			// we can NOT proceed without a file name!!
+			if ($logging) {
+				echo "Error: csv2pg: Missing file name. \n";
+			}
+			if ($debugging) {
+				echo "Options array: csv2pg:\n";
+				print_r($options);
+			}
+			return false;
+		}
+	}
+	/*
+	 * get the method arg
+	 */
+	if (array_key_exists(METHOD,$options)) {
+		$method = $options[METHOD];
+	} else {
+		// we can set a default for this
+		$method = 1;
+	}
+	if ($debugging) echo "method default: $method \n";
+	$method_arg = getargs ("method",$method);
+	if ($debugging) echo "method_arg: $method_arg \n";
+	if (strlen(trim($method_arg))) {
+		$method = intval($method_arg);
+		$options[METHOD] = $method;
+	}
+	if ($debugging) echo "logging final: $logging \n";
+	/*
+	 * get the skiplines arg
+	 */
+	if (array_key_exists(SKIPLINES,$options)) {
+		$skiplines = $options[SKIPLINES];
+	} else {
+		// we can set a default for this
+		$skiplines = 0;
+	}
+	if ($debugging) echo "skiplines default: $skiplines \n";
+	$skiplines_arg = getargs ("skiplines",$skiplines);
+	if ($debugging) echo "skiplines_arg: $skiplines_arg \n";
+	if (strlen(trim($skiplines_arg))) {
+		$skiplines = intval($skiplines_arg);
+		$options[SKIPLINES] = $skiplines;
+	}
+	if ($debugging) echo "skiplines final: $skiplines \n";
+	/*
+	 * get the fieldcount arg
+	 */
+	if (array_key_exists(FIELDCOUNT,$options)) {
+		$fieldcount = $options[FIELDCOUNT];
+	} else {
+		// we can set a default for this, 0 means unknown
+		$fieldcount = 0;
+	}
+	if ($debugging) echo "fieldcount default: $fieldcount \n";
+	$fieldcount_arg = getargs ("fieldcount",$fieldcount);
+	if ($debugging) echo "fieldcount_arg: $fieldcount_arg \n";
+	if (strlen(trim($fieldcount_arg))) {
+		$fieldcount = intval($fieldcount_arg);
+		$options[FIELDCOUNT] = $fieldcount;
+	}
+	if ($debugging) echo "fieldcount final: $fieldcount \n";
+	/*
+	 * get the linenumbers arg
+	 */
+	if (array_key_exists(LINENUMBERS,$options)) {
+		$linenumbers = $options[LINENUMBERS];
+	} else {
+		$linenumbers = false;
+	}
+	if ($debugging) echo "linenumbers default: $linenumbers \n";
+	$linenumbers_arg = getargs ("linenumbers",$linenumbers);
+	if ($debugging) echo "linenumbers_arg: $linenumbers_arg \n";
+	if (strlen(trim($linenumbers_arg))) {
+		$linenumbers = strtobool($linenumbers_arg);
+		$options[LINENUMBERS] = $linenumbers;
+	}
+	if ($debugging) echo "linenumbers final: $linenumbers \n";
+	
+	/*
+	 * now start the file processing
+	 * convert file into array of file records
+	 */
 	if ($file_records = file($file_name)) {
 		if ($debugging) {
-			echo "File record array: csv2pg:\n";
+			echo "File record array: csv2pg: \n";
 			print_r($file_records);
 		}
 		/*
