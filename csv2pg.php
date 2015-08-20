4144071657ecaf0884619900b18ba4b14066ef80
Jim Brannon <jim@brannondevelopments.com>
2015-08-20 00:00:30 -0600

    working simple csv to array codeand added example files

--- csv2pg.php
+++ csv2pg.php
@@ -1,13 +1,16 @@
 <?php
+$file_name="dirty_data_1.txt";
+print_r(csv2pg($file_name));
 
-function csv2pg($file,$options=array()) {
-	
+function csv2pg($file_name,$options=array()) {
+	return csv2array(file($file_name),$options);
 }
 
-function csv2array($file,$skipline=0,$method=0) {
+function csv2array($file_records,$options=array()) {
+	$method=0;
 	switch($method) {
 		case 0:
 		default:
-			return array_map('str_getcsv', file('data.csv'));
+			return array_map('str_getcsv', $file_records);
 	}
 }
